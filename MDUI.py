834a6cee12dd5c6e36f348f72a7111281a8aa4c2
zhouzhouonmac <32453952+zhouzhou514@users.noreply.github.com>
2018-11-07 18:23:47 +0800

    it work!

--- MDUI.py
+++ MDUI.py
@@ -7,14 +7,17 @@ def detect_video():
     history = 10    # 训练帧数
     wid = camera.get(3)    # 1280:video resolution
     hgh = camera.get(4)    # 720:video resolution
-    rectLoc1 =[(int(wid*0.70),int(hgh*0.65)),(int(wid*0.95),int(hgh*0.93))]
+    rectLoc1 =[(int(wid*0.70),int(hgh*0.65)), (int(wid*0.95),int(hgh*0.93))]
     textOrg1 =(int(wid*0.72),int(hgh*0.80))
+    textOrg01 =(int(wid*0.72),int(hgh*0.))
 
-    rectLoc2 =[(int(wid*0.70),int(hgh*0.35)),(int(wid*0.95),int(hgh*0.07))]
+
+    rectLoc2 =[(int(wid*0.70),int(hgh*0.35)), (int(wid*0.95),int(hgh*0.07))]
     textOrg2 =(int(wid*0.72),int(hgh*0.25))
 
     rectLoc3 = [(int(wid * 0.05), int(hgh * 0.10)), (int(wid * 0.20), int(hgh * 0.30))]
     textOrg3 = (int(wid * 0.10), int(hgh * 0.25))
+    textOrg03 = (int(wid * 0.10), int(hgh * 0.25))
 
     bs = cv2.createBackgroundSubtractorKNN(detectShadows=True)  # 背景减除器，设置阴影检测
     bs.setHistory(history)
@@ -40,10 +43,10 @@ def detect_video():
         # 获取所有检测框
         image, contours, hier = cv2.findContours(dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         cv2.rectangle(frame,rectLoc1[0],rectLoc1[1], (255, 255, 255), 5)
-        cv2.putText(frame,"DAISUKE",textOrg1,2,2,(255,255,255),3)
+        cv2.putText(frame,"JOB1",textOrg1,2,2,(255,255,255),3)
 
         cv2.rectangle(frame,rectLoc2[0],rectLoc2[1], (255, 255, 255), 5)
-        cv2.putText(frame,"DAISUKE",textOrg2,2,2,(255,255,255),3)
+        cv2.putText(frame,"JOB2",textOrg2,2,2,(255,255,255),3)
 
         cv2.rectangle(frame, rectLoc3[0], rectLoc3[1], (255, 255, 255), 5)
         cv2.putText(frame, "ESC", textOrg3, 2, 2, (255, 255, 255), 3)
@@ -56,8 +59,16 @@ def detect_video():
             area = cv2.contourArea(c)
             if MINCON < area < MAXCON:
                 cv2.rectangle(frame, (rect[0], rect[1]), (rect[0] + rect[2], rect[1] + rect[3]), (0, 255, 0), 1)
-                if rectContains(rect,(int(wid * 0.125), int(hgh * 0.20))):
-                    job1()
+                if rectContains(rectLoc3,(int((rect[0] + rect[2])/2), int((rect[1] + rect[3])/2))):  #job3
+                    global j3cntr
+                    j3cntr+=1
+
+            cv2.putText(frame, str('%.3f'%(j3cntr/THRESHOLD)), textOrg03, 2, 1, (0, 255, 255), 3)  #show the current percentage to thershold
+
+            if (j3cntr >= THRESHOLD):
+                job3()
+                j3cntr = 0
+
 
 
         cv2.imshow("detection", frame)
@@ -66,7 +77,7 @@ def detect_video():
             break
     camera.release()
 
-def job1():
+def job3():
     global cntr
     cntr += 1
     #time.sleep(1)
@@ -74,16 +85,16 @@ def job1():
 
 
 def rectContains(rect,pt):#return true if point is in rectangle
-    logic = rect[0] < pt[0] < rect[0]+rect[2] and rect[1] < pt[1] < rect[1]+rect[3]
+    logic = rect[0][0] < pt[0] < rect[1][0] and rect[0][1] < pt[1] < rect[1][1]
     return logic
 
 
 if __name__ == '__main__':
-    global cntr
-    global THRESHOLD
-    THRESHOLD = 1000
+    global THRESHOLD,j3cntr,cntr
+    j3cntr = 0
+    THRESHOLD = 100
     cntr = 0
     global MINCON,MAXCON
-    MINCON = 200
+    MINCON = 5000
     MAXCON = 60000
     detect_video()
